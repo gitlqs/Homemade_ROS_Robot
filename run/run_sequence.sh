e7591aca9ac5b463473a6556182ae9972a79a7e0
gitlqs <fruitlqs@gmail.com>
2019-08-12 19:28:40 -0400

    modified bash script

--- run/run_sequence.sh
+++ run/run_sequence.sh
@@ -1,2 +1,10 @@
-#!/bin/bash
-x-terminal-emulator -e ./sequence/visualize_urdf.sh
+#/bin/bash
+sudo chmod 666 /dev/ttyACM0
+sudo chmod 666 /dev/ttyUSB0
+gnome-terminal -e ./sequence/run_roscore.sh
+sleep 5
+gnome-terminal -e ./sequence/run_rosserial.sh
+sleep 4
+gnome-terminal -e ./sequence/bringup.sh
+sleep 5
+gnome-terminal -e ./sequence/run_rtabmap.sh
---
+++ run/sequence/run_rtabmap.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/OpenAGV/catkin_ws/devel/setup.bash
+roslaunch openagv_navigation rtabmap.launch
