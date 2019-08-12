12cc24cb26e3af7cfd291b076ed572022834669e
gitlqs <fruitlqs@gmail.com>
2019-08-12 02:13:40 -0400

    add run folder to store all bash script to run ros command (automated)

---
+++ run/run_sequence.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+x-terminal-emulator -e ./sequence/visualize_urdf.sh
---
+++ run/sequence/visualize_urdf.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+source ~/OpenAGV/catkin_ws/devel/setup.bash
+roslaunch openagv_description visualize_urdf.launch
