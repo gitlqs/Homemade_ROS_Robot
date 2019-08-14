0880ebed092a62f2ebc23897d3e5e65ac6db753b
gitlqs <fruitlqs@gmail.com>
2019-08-14 18:43:59 -0400

    changed the talker function

--- catkin_ws/src/send_ssvep_data/src/send_ssvep_data.py
+++ catkin_ws/src/send_ssvep_data/src/send_ssvep_data.py
@@ -3,20 +3,22 @@
 import rospy
 from geometry_msgs.msg import Twist
 
+
 def talker(linearX, angularZ):
     pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
     rospy.init_node('ssvep_data_sender', anonymous=True)
     rate = rospy.Rate(10) # 10hz
-    while not rospy.is_shutdown():
-        msg = Twist()
-        msg.linear.x = linearX
-        msg.linear.y = 0
-        msg.linear.z = 0
-        msg.angular.x = 0
-        msg.angular.y = 0
-        msg.angular.z = angularZ
-        pub.publish(msg)
-        rate.sleep()
+
+    msg = Twist()
+    msg.linear.x = linearX
+    msg.linear.y = 0
+    msg.linear.z = 0
+    msg.angular.x = 0
+    msg.angular.y = 0
+    msg.angular.z = angularZ
+    pub.publish(msg)
+    rate.sleep()
+
 
 if __name__ == '__main__':
     try:
